92aa0722598d90273a49f9196be4c6bc1c8bfd8c
Speeen <87054682+LandonThePancake@users.noreply.github.com>
2023-11-01 16:45:59 -0600

    added the files to github

---
+++ DualFishInterpreter.py
@@ -0,0 +1,70 @@
+print("----------=Dualfish Interpreter V0.7=----------\n <==> Type 'help' for a list of commands! <==>")
+#iic**iiarii*ia>iii*i<+aa>dddd<+a>dddaiiiciiiiiii<+ariiii**da>iii<+a>*<-arii*ae
+#Pre defined values
+Register1 = 0
+Register2 = 0
+RegisterChoice = 0 #current register
+RegisterAlt = 1 #non selected register
+SelectedRegister = [Register1, Register2]
+Alphabet = [" ","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",]
+run = True
+while run:
+    print("")
+    User_Input = input("DuFi>> ").lower()
+    if User_Input == "help":
+        print("Commands:\ni = increment current register by 1\nd = decrement current register by 1\ns = square current register\nc = cube current register\n* = double current register\n+ = add registers together (into the selected register)\n- = subtract unselected register from selected register\no = output selected register\n0 = output both registers side by side\na = output alphabetical character (space is 0) (1-26 lowercase) (27 - 52 uppercase)\n< = switch to register1\n> = switch to register2\nr = resets both registers to 0\ne = stop/end the program")
+    else:
+        #goes through each character and gives it a function
+        #if no function exists, it throws a warning message but continues the code
+        for i in User_Input:
+            if i == "i": #increment reg
+                SelectedRegister[RegisterChoice] += 1
+            elif i == "d": #decrement reg
+                SelectedRegister[RegisterChoice] -= 1
+            elif i == "s": #square reg
+                SRvalue = SelectedRegister[RegisterChoice]
+                SelectedRegister[RegisterChoice] = SRvalue * SRvalue
+                #SelectedRegister[RegisterChoice] *= SelectedRegister[RegisterChoice]
+            elif i == "c": #cube reg
+                SRvalue = SelectedRegister[RegisterChoice]
+                SelectedRegister[RegisterChoice] = SRvalue * SRvalue * SRvalue
+            elif i == "o": #output selected reg
+                print(SelectedRegister[RegisterChoice], end="")
+            elif i == "0": #output both reg
+                print(SelectedRegister)
+            elif i == "a": #output alphabetical character from selected reg
+                SRvalue = SelectedRegister[RegisterChoice]
+                print(Alphabet[SRvalue], end="")
+            elif i == "*": #double selected reg
+                SelectedRegister[RegisterChoice] = SelectedRegister[RegisterChoice]*2
+            elif i == "+": #add reg values
+                SRvalue = SelectedRegister[RegisterChoice]
+                ARvalue = SelectedRegister[RegisterAlt]
+                SelectedRegister[RegisterChoice] = SRvalue + ARvalue
+            elif i == "-": #subtract reg values
+                SRvalue = SelectedRegister[RegisterChoice]
+                ARvalue = SelectedRegister[RegisterAlt]
+                SelectedRegister[RegisterChoice] = SRvalue - ARvalue
+            elif i == "f": #converts the register from negative to positive, or vise versa
+                SRvalue = SelectedRegister[RegisterChoice]
+                if SRvalue < 0:
+                    SRvalue = abs(SRvalue)
+                elif SRvalue > 0:
+                    SRvalue = -abs(SRvalue)
+                else:
+                    pass
+                SelectedRegister[RegisterChoice] = SRvalue
+            elif i == "<": #set reg to 1
+                RegisterChoice = 0
+                RegisterAlt = 1
+            elif i == ">": #set reg to 2
+                RegisterChoice = 1
+                RegisterAlt = 0
+            elif i == "r": #reset values
+                SelectedRegister = [Register1, Register2]
+            elif i == "e": #end/exit program
+                run = False
+            elif i == " " or "  ": #ignore whitespace
+                pass
+            else: #throw an error
+                print("Unrecognized Character Detected!")
